2c45624fb94e01e4235bea6080ca722ddbe03b9f
Nathan Stucke <stucke03@cairo.cs.colostate.edu>
2026-04-03 14:34:39 -0600

    added header to index.php and head

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 /* Modern-looking design with your company name and logo.
 A welcome message explaining what the site is about.
 Navigation links to all other pages on the site. */
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>ColorTheory homepage</title>
+    <meta name="authors" content="Nathan Stucke, Morgan Mitchell">
+    <meta name="description" content="A homepage for the ColorTheory website">
+    <meta name="keywords" content="Webpage, HTML5, Nathan Stucke, Morgan Mitchell, ColorTheory, CS312, Web Development, Colorado State University">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>ColorTheory</h1>
+    </header>
+</body>
+
+</html>
