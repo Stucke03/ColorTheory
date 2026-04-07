22e76c366e1d0d5845cc458d62b23b8dda9a0658
Nathan Stucke <stucke03@cairo.cs.colostate.edu>
2026-04-06 18:24:50 -0600

    got started on home page design

--- index.php
+++ index.php
@@ -22,6 +22,75 @@ Navigation links to all other pages on the site.-->
         <a href="color.php">Color Coordinates</a>
     </header>
     <hr>
+    <main>
+        <style>
+            .square-outer {
+            height: 50vh;
+            width: 120vh;
+            background-color: white;
+            bottom: 0;
+            position: fixed;
+            }
+            .square-middle-out {
+            background-color: #EB8258;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            height: 100%;
+            width: 98%;
+            }
+            .square-middle-in {
+            background-color: white;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            height: 100%;
+            width: 90%;
+            }
+
+            .oval-outer {
+            height: 500px;
+            width: 120vh;
+            background-color: white;
+            position: fixed;
+            border-radius: 50%;
+            bottom: 18%;
+            }
+            .oval-middle-out {
+            height: 95%;
+            width: 98%;
+            background-color: #EB8258;
+            position: absolute;
+            border-radius: 50%;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            }
+            .oval-middle-in {
+            height: 95%;
+            width: 98%%;
+            background-color: white;
+            position: absolute;
+            border-radius: 50%;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            }
+        </style>
+        <div class="oval-outer">
+            <div class="oval-middle-out">
+                <div class="oval-middle-in"></div>
+            </div>
+        </div>
+        <div class="square-outer">
+            <div class="square-middle-out">
+                <div class="square-middle-in"></div>
+            </div>
+        </div>
+        
+    </main>
 </body>
 
 </html>
