db58dfac8abbd9c4545120cafbce4f47814b917f
Nathan Stucke <stucke03@concord.cs.colostate.edu>
2026-05-03 18:07:32 -0600

    added vars back to db.php

--- db.php
+++ db.php
@@ -1,4 +1,9 @@
 <?php
+$host = "";
+$username = "";   // e.g. arefin or C8xxxxxxx
+$password = ""; // your CS Linux password
+$database = "";   // same as username
+
 $conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
